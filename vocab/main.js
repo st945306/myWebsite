b403f941e19a3cf1c0ca805fe55e46b3aa868c8b
st945306 <st945306@gmail.com>
2016-01-29 00:32:48 +0800

    color on right ans

--- vocab/main.js
+++ vocab/main.js
@@ -1,21 +1,30 @@
 var book = JSON.parse(book);
 var vocaNum = book.length;
+var green = '#51a351';
+var red = '#bd362f';
 
 var question = document.getElementById("question");
 var options = [];
 for (var i = 1; i <= 4; i++)
 	options[i] = document.getElementById("option" + i);
 
+var ans;
 makeQuestion();
 
 
 function pick(id){
-	alert("button " + id + " was clicked");
+//	alert("button " + id + " was clicked");
+	options[ans].style.backgroundColor = green;
+	if (id != ans)
+		options[id].style.backgroundColor = red;
+	setTimeout(makeQuestion, 1500);
 }
 
 function makeQuestion(){
-	alert(vocaNum);
 	question.innerHTML = book[0].en;
-	for (var i = 1; i <= 4; i++)
+	for (var i = 1; i <= 4; i++){
 		options[i].innerHTML = book[i].ch;
+		options[i].style.backgroundColor = 'white';
+	}
+	ans = 1;
 }
