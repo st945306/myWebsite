2e6452f217f9d11704548830b5b8cc47cc5d49fb
st945306 <st945306@gmail.com>
2016-01-29 00:08:29 +0800

    get vocaNum

--- vocab/main.js
+++ vocab/main.js
@@ -1,16 +1,21 @@
-var data = JSON.parse(book);
+var book = JSON.parse(book);
+var vocaNum = book.length;
 
 var question = document.getElementById("question");
 var options = [];
 for (var i = 1; i <= 4; i++)
 	options[i] = document.getElementById("option" + i);
 
+makeQuestion();
 
-question.innerHTML = data[0].en;
-for (var i = 1; i <= 4; i++){
-	options[i].innerHTML = data[i].ch;
-}
 
 function pick(id){
 	alert("button " + id + " was clicked");
+}
+
+function makeQuestion(){
+	alert(vocaNum);
+	question.innerHTML = book[0].en;
+	for (var i = 1; i <= 4; i++)
+		options[i].innerHTML = book[i].ch;
 }
